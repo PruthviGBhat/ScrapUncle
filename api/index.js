9da3f2dd523cae62677ec1601697f768236f6b4b
Pruthvi Bhat <pruthvigbhaveri@gmail.com>
2024-03-03 22:26:19 +0530

    added build path

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser';
 import Userrouter from './routes/user.route.js';
 import Authrouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
-
+import path from 'path';
 dotenv.config();
 mongoose.connect(process.env.MONGO)
 .then(()=>{
@@ -17,6 +17,7 @@ mongoose.connect(process.env.MONGO)
 
 const app = express()
 const port = 3000
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(express.json());
@@ -25,6 +26,12 @@ app.use("/api/user",Userrouter);
 app.use("/api/auth",Authrouter);
 app.use('/api/listing', listingRouter);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+})
 
 app.use((err,req,res,next)=>{
     console.log(err)
